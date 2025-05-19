86214adcdc82c3a436fc9f7e76928ad620fb66ed
Larissa Queiroz <larissa.queiroz95@gmail.com>
2025-05-19 18:55:12 -0300

    add create user and login tests

--- UserService.Tests/UserServiceTests.cs
+++ UserService.Tests/UserServiceTests.cs
@@ -6,42 +6,50 @@ using System.ComponentModel;
 using System.Net;
 using System.Text;
 using UserService.Models.DAO;
+using UserService.Models.DTO;
+using UserService.Tests.Utils;
 
 namespace UserService.Tests
 {
     public class UserServiceTests: IClassFixture<IntegrationTestWebAppFactory>
     {
 
-        private readonly HttpClient _client;
-        private readonly Uri requestUri;
+        private readonly LoginHelper _loginHelper;
         public UserServiceTests(IntegrationTestWebAppFactory factory)
         {
-            _client = factory.CreateClient();
-
-            requestUri = new UriBuilder(Uri.UriSchemeHttp, "localhost", factory.GetPort(), "scalar/v1").Uri;
+            _loginHelper = new LoginHelper(factory);
         }
 
 
         [Fact]
         public async Task CreateMeter_ShouldCreateProductAsync()
         {
-            var user = new User
+            var user = new UserDto()
             {
                 Email = "test@email.com",
+                Password = "test@123",
                 Name = "Test",
-                Hash = new PasswordHasher<User>().HashPassword(new User(), "test")
+                Type = Enum.UserType.USER,
             };
 
             var content = new StringContent(JsonConvert.SerializeObject(user), Encoding.UTF8, "application/json");
 
-            var response = await _client.PostAsync("/scalar/v1/api/users", content);
+            var response = await _loginHelper._client.PostAsync("/scalar/v1/users", content);
 
             var body = await response.Content.ReadAsStringAsync();
-            var createdUser = JsonConvert.DeserializeObject<User>(body);
+            var createdUser = JsonConvert.DeserializeObject<UserDto>(body);
 
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
             Assert.NotNull(createdUser);
 
+        }
+
+        [Fact]
+        public async Task Login_ShouldReturnToken()
+        {
+            var result = _loginHelper.GetToken();
+
+            Assert.NotNull(result);
         }
     }
 }
---
+++ UserService.Tests/Utils/LoginHelper.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserService.Models.DTO;
+
+namespace UserService.Tests.Utils
+{
+    
+    public class LoginHelper
+    {
+        public HttpClient _client;
+
+        public LoginHelper(IntegrationTestWebAppFactory factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        public string GetToken()
+        {
+            var login = new UserLoginDto
+            {
+                Email = "admin@email.com",
+                Password = "Admin@@123"
+            };
+
+            var content = new StringContent(JsonConvert.SerializeObject(login), Encoding.UTF8, "application/json");
+
+            var result = _client.PostAsync("/scalar/v1/users/login", content).Result;
+
+            var token = result.Content.ReadAsStringAsync().Result;
+
+            return token;
+        }
+    }
+}
